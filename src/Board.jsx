7777011664df1d0ecc4ae6be2e793d4ecf387be5
OmkarKandale <omkarkandale@gmail.com>
2021-07-13 23:36:54 +0530

    added logic for black squares on the board

--- src/Board.jsx
+++ src/Board.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import BoardSquare from "./BoardSquare";
 
 const Board = ({ board }) => {
+	const getXYPosition = (i) => {
+		const x = i % 8;
+		const y = Math.abs(Math.floor(i / 8) - 7);
+		return { x, y };
+	};
+	const isBlack = (i) => {
+		const { x, y } = getXYPosition(i);
+		return (x + y) % 2 === 1;
+	};
 	return (
 		<div className="board">
 			{board.flat().map((piece, i) => (
 				<div key={i} className="square">
-					<BoardSquare piece={piece} />
+					<BoardSquare piece={piece} black={isBlack(i)} />
 				</div>
 			))}
 		</div>
