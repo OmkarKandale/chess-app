e73049a0230fa94060a2ecd8c5b4444c4618a6d6
OmkarKandale <omkarkandale@gmail.com>
2021-07-14 00:05:18 +0530

    added react-dnd

--- src/Piece.jsx
+++ src/Piece.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useDrag } from "react-dnd";
 
 const Piece = ({ piece: { type, color } }) => {
+	const [, drag] = useDrag({
+		type: "piece",
+		item: { id: `${type}_${color}` },
+	});
+
 	const pieceImg = `./assets/${type}_${color}.png`;
+
 	return (
-		<div className="piece-container">
+		<div className="piece-container" ref={drag}>
 			<img src={pieceImg} alt="" className="piece" />
 		</div>
 	);
