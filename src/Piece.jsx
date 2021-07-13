5d1586d8a4713981ca31244197d1e5b734c533c6
OmkarKandale <omkarkandale@gmail.com>
2021-07-13 23:51:01 +0530

    added styles

--- src/Piece.jsx
+++ src/Piece.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Piece = ({ piece: { type, color } }) => {
 	const pieceImg = `./assets/${type}_${color}.png`;
 	return (
-		<div>
-			<img src={pieceImg} alt="" />
+		<div className="piece-container">
+			<img src={pieceImg} alt="" className="piece" />
 		</div>
 	);
 };
