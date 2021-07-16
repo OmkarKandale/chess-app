69e758a2b4d0c3d6564bab238809229539213678
OmkarKandale <omkarkandale@gmail.com>
2021-07-17 00:04:33 +0530

    added UI to show Game Over and result

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,33 @@ import { gameSubject, initGame } from "./Game";
 
 const App = () => {
 	const [board, setBoard] = useState([]);
+	const [isGameOver, setIsGameOver] = useState();
+	const [result, setResult] = useState();
 
 	useEffect(() => {
 		initGame();
-		const subscribe = gameSubject.subscribe((game) => setBoard(game.board));
+		const subscribe = gameSubject.subscribe((game) => {
+			setBoard(game.board);
+			setIsGameOver(game.isGameOver);
+			setResult(game.result);
+		});
 		return () => subscribe.unsubscribe();
 	}, []);
 
 	return (
 		<div className="container">
+			{isGameOver && (
+				<h2 className="vertical-text">
+					GAME OVER
+					<button>
+						<span className="vertical-text">NEW GAME</span>
+					</button>
+				</h2>
+			)}
 			<div className="board-container">
 				<Board board={board} />
 			</div>
+			{result && <p className="vertical-text">{result}</p>}
 		</div>
 	);
 };
